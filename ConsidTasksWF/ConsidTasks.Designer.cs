93dd75aec8ef5b2cb5c077ded7d39abd3ee37864
PER JANSSON <jur12pja@lu.se>
2017-03-22 19:14:32 +0100

    Add project files.

---
+++ ConsidTasksWF/ConsidTasks.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace ConsidTasksWF
+{
+    partial class ConsidTasks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label_Search_Results = new System.Windows.Forms.Label();
+            this.label_unaccesibleDirectories = new System.Windows.Forms.Label();
+            this.label_accesibleFiles = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_Begin_Search = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Selected_Directory = new System.Windows.Forms.TextBox();
+            this.button_Select_Directory = new System.Windows.Forms.Button();
+            this.label_desc = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button_convert_Xml = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_Selected_Xml_File = new System.Windows.Forms.TextBox();
+            this.button_select_Xml_File = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label_Conversion_Result = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(382, 348);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label_Search_Results);
+            this.tabPage1.Controls.Add(this.label_unaccesibleDirectories);
+            this.tabPage1.Controls.Add(this.label_accesibleFiles);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.button_Begin_Search);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox_Selected_Directory);
+            this.tabPage1.Controls.Add(this.button_Select_Directory);
+            this.tabPage1.Controls.Add(this.label_desc);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(374, 322);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Task 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label_Search_Results
+            // 
+            this.label_Search_Results.AutoSize = true;
+            this.label_Search_Results.Location = new System.Drawing.Point(6, 167);
+            this.label_Search_Results.Name = "label_Search_Results";
+            this.label_Search_Results.Size = new System.Drawing.Size(0, 13);
+            this.label_Search_Results.TabIndex = 11;
+            // 
+            // label_unaccesibleDirectories
+            // 
+            this.label_unaccesibleDirectories.AutoSize = true;
+            this.label_unaccesibleDirectories.Location = new System.Drawing.Point(176, 244);
+            this.label_unaccesibleDirectories.Name = "label_unaccesibleDirectories";
+            this.label_unaccesibleDirectories.Size = new System.Drawing.Size(0, 13);
+            this.label_unaccesibleDirectories.TabIndex = 10;
+            // 
+            // label_accesibleFiles
+            // 
+            this.label_accesibleFiles.AutoSize = true;
+            this.label_accesibleFiles.Location = new System.Drawing.Point(235, 210);
+            this.label_accesibleFiles.Name = "label_accesibleFiles";
+            this.label_accesibleFiles.Size = new System.Drawing.Size(0, 13);
+            this.label_accesibleFiles.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Number of unaccesible directories:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(229, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Number of accesible xml files in given directory:";
+            // 
+            // button_Begin_Search
+            // 
+            this.button_Begin_Search.Location = new System.Drawing.Point(6, 112);
+            this.button_Begin_Search.Name = "button_Begin_Search";
+            this.button_Begin_Search.Size = new System.Drawing.Size(145, 23);
+            this.button_Begin_Search.TabIndex = 5;
+            this.button_Begin_Search.Text = "Begin search";
+            this.button_Begin_Search.UseVisualStyleBackColor = true;
+            this.button_Begin_Search.Click += new System.EventHandler(this.begin_Search_Btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Chosen directory:";
+            // 
+            // textBox_Selected_Directory
+            // 
+            this.textBox_Selected_Directory.Location = new System.Drawing.Point(6, 77);
+            this.textBox_Selected_Directory.Name = "textBox_Selected_Directory";
+            this.textBox_Selected_Directory.ReadOnly = true;
+            this.textBox_Selected_Directory.Size = new System.Drawing.Size(145, 20);
+            this.textBox_Selected_Directory.TabIndex = 3;
+            // 
+            // button_Select_Directory
+            // 
+            this.button_Select_Directory.Location = new System.Drawing.Point(6, 25);
+            this.button_Select_Directory.Name = "button_Select_Directory";
+            this.button_Select_Directory.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button_Select_Directory.Size = new System.Drawing.Size(145, 23);
+            this.button_Select_Directory.TabIndex = 2;
+            this.button_Select_Directory.Text = "Select directory";
+            this.button_Select_Directory.UseVisualStyleBackColor = true;
+            this.button_Select_Directory.Click += new System.EventHandler(this.select_Directory_Btn_Click);
+            // 
+            // label_desc
+            // 
+            this.label_desc.AutoSize = true;
+            this.label_desc.Location = new System.Drawing.Point(3, 3);
+            this.label_desc.Name = "label_desc";
+            this.label_desc.Size = new System.Drawing.Size(245, 13);
+            this.label_desc.TabIndex = 1;
+            this.label_desc.Text = "Please select which directory to begin search with:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label_Conversion_Result);
+            this.tabPage2.Controls.Add(this.button_convert_Xml);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.textBox_Selected_Xml_File);
+            this.tabPage2.Controls.Add(this.button_select_Xml_File);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(374, 322);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Task 2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button_convert_Xml
+            // 
+            this.button_convert_Xml.Location = new System.Drawing.Point(6, 113);
+            this.button_convert_Xml.Name = "button_convert_Xml";
+            this.button_convert_Xml.Size = new System.Drawing.Size(156, 23);
+            this.button_convert_Xml.TabIndex = 4;
+            this.button_convert_Xml.Text = "Convert and Save File  ";
+            this.button_convert_Xml.UseVisualStyleBackColor = true;
+            this.button_convert_Xml.Click += new System.EventHandler(this.button_convert_Xml_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Chosen File:";
+            // 
+            // textBox_Selected_Xml_File
+            // 
+            this.textBox_Selected_Xml_File.Location = new System.Drawing.Point(6, 78);
+            this.textBox_Selected_Xml_File.Name = "textBox_Selected_Xml_File";
+            this.textBox_Selected_Xml_File.ReadOnly = true;
+            this.textBox_Selected_Xml_File.Size = new System.Drawing.Size(156, 20);
+            this.textBox_Selected_Xml_File.TabIndex = 2;
+            // 
+            // button_select_Xml_File
+            // 
+            this.button_select_Xml_File.Location = new System.Drawing.Point(6, 25);
+            this.button_select_Xml_File.Name = "button_select_Xml_File";
+            this.button_select_Xml_File.Size = new System.Drawing.Size(156, 23);
+            this.button_select_Xml_File.TabIndex = 1;
+            this.button_select_Xml_File.Text = "Select File";
+            this.button_select_Xml_File.UseVisualStyleBackColor = true;
+            this.button_select_Xml_File.Click += new System.EventHandler(this.button_select_Xml_File_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Select XML file to convert:";
+            // 
+            // label_Conversion_Result
+            // 
+            this.label_Conversion_Result.AutoSize = true;
+            this.label_Conversion_Result.Location = new System.Drawing.Point(6, 159);
+            this.label_Conversion_Result.Name = "label_Conversion_Result";
+            this.label_Conversion_Result.Size = new System.Drawing.Size(0, 13);
+            this.label_Conversion_Result.TabIndex = 5;
+            // 
+            // ConsidTasks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 367);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "ConsidTasks";
+            this.Text = "ConsidTasksWF - Per Jansson";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label_desc;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button_Select_Directory;
+        private System.Windows.Forms.TextBox textBox_Selected_Directory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_Begin_Search;
+        private System.Windows.Forms.Label label_unaccesibleDirectories;
+        private System.Windows.Forms.Label label_accesibleFiles;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_Search_Results;
+        private System.Windows.Forms.Button button_select_Xml_File;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_convert_Xml;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_Selected_Xml_File;
+        private System.Windows.Forms.Label label_Conversion_Result;
+    }
+}
+
---
+++ ConsidTasksWF/ConsidTasks.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ConsidTasksWF
+{
+    public partial class ConsidTasks : Form
+    {
+        public ConsidTasks()
+        {
+            InitializeComponent();
+        }
+
+        ///-------------------------------------------------------------------------
+        ///                           TASK 1 
+        ///-------------------------------------------------------------------------
+
+        /// <summary>
+        /// THIS BUTTON PROMTS YOU TO SELECT A FOLDER.
+        /// </summary>
+               
+        private void select_Directory_Btn_Click(object sender, EventArgs e)
+        {   
+            //Saves the selected folder into a string in the textbox when the user presses "OK".
+            FolderBrowserDialog fBD = new FolderBrowserDialog();
+            if (fBD.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox_Selected_Directory.Text = fBD.SelectedPath;
+            }
+        }
+
+        /// <summary>
+        /// THIS BUTTON BEGINS THE SEARCH OF XML FILES IN THE GIVEN DIRECTORY AND ALL SUBDIRECTORIES.
+        /// </summary>
+
+        private void begin_Search_Btn_Click(object sender, EventArgs e)
+        {
+            //First we clear all lables from previous searches.
+            label_accesibleFiles.Text = "";
+            label_unaccesibleDirectories.Text = "";
+            label_Search_Results.Text = "";
+
+            //Checks if the user has selected a folder, otherwise promts user to do so.
+            if (textBox_Selected_Directory.Text == "")
+            {
+                textBox_Selected_Directory.Text = "Please select directorty first";
+            }
+            else
+            {
+                //Shows the user that the program is processing.
+                Cursor.Current = Cursors.WaitCursor;
+               
+                //Create our counters that will be referenced.
+                int accessCount = 0;
+                int deniedCount = 0;
+
+                //We call on the method that will count the XML files.
+                GetAllXmlFiles(textBox_Selected_Directory.Text, ref accessCount, ref deniedCount); 
+                
+                //We present the result into our labels and tell the user that the search has finished.
+                label_accesibleFiles.Text = accessCount.ToString();
+                label_unaccesibleDirectories.Text = deniedCount.ToString();
+                label_Search_Results.Text = "Search finished!";
+
+            }
+        }
+
+        /// <summary>
+        /// THIS METHOD SEARCHES TO FIND XML FILES IN THE GIVEN DIRECTORY AND ALL SUBDIRECTORIES.
+        /// </summary>
+        /// <param name="pathDir">The name of the directory we're currently in</param>
+        /// <param name="accessCount">Int which will count the number of found XML-files</param>
+        /// <param name="deniedCount">Int which will count the number of failed directory entries because of denied permission to access them</param>
+
+        public static void GetAllXmlFiles(string pathDir, ref int accessCount, ref int deniedCount)
+        {
+            try
+            {
+                //We search the directory we're currently in for files with the XML extension.
+                foreach (string file in Directory.GetFiles(pathDir, "*.xml"))
+                {
+                    //Increase int by one for each file found.
+                    accessCount++;
+                }
+            }
+            
+            catch (Exception Error)
+            {
+                //Any exception is caught and printed in console.
+                Console.WriteLine(Error.Message);                
+            }
+            //We search the directory we're currently in for any subdirectories.
+            foreach (string dir in Directory.GetDirectories(pathDir))
+            {
+                try
+                {   
+                    //For each subdirectory found, we will recursively call on the same method we're currently in to search each one for XML files.
+                    GetAllXmlFiles(dir, ref accessCount, ref deniedCount);
+                }
+                catch (Exception Error)
+                {
+                    //Any exception is caught and printed in console. Increase int by one for each entry denied.
+                    Console.WriteLine(Error.Message);
+                    deniedCount++;
+                }
+            }
+
+                      
+        }
+
+        ///-------------------------------------------------------------------------
+        ///                           TASK 2 
+        ///-------------------------------------------------------------------------
+
+        /// <summary>
+        /// THIS BUTTON PROMTS YOU TO SELECT A FILE FOR CONVERSION.
+        /// </summary>    
+
+        private void button_select_Xml_File_Click(object sender, EventArgs e)
+        {
+            //Clear result from previous searches.
+            label_Conversion_Result.Text = "";
+
+            OpenFileDialog oFD = new OpenFileDialog();
+            if (oFD.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //Saves the selected file into a string in the textbox when the user presses "OK".
+                textBox_Selected_Xml_File.Text = oFD.FileName;
+            }
+
+        }
+
+        /// <summary>
+        /// THIS BUTTON BEGINS THE CONVERSION OF THE SELECTED XML FILE.
+        /// </summary>
+
+        private void button_convert_Xml_Click(object sender, EventArgs e)
+        {
+            //Checks if the user has selected a file, otherwise promts user to do so.
+            if (textBox_Selected_Xml_File.Text == "")
+            {
+                textBox_Selected_Xml_File.Text = "Please select file first";
+            }
+            //Checks if the user has selected a file the program is able to convert, otherwise promts user to do so.
+            else if (!textBox_Selected_Xml_File.Text.Contains("CustomerOrders.xml"))
+            {
+                textBox_Selected_Xml_File.Text = "Please select correct xml file";
+            }
+            else
+            {
+                //Create a XML document for us to work with and load the selected document into our variable.
+                XmlDocument xmlTest = new XmlDocument();
+                xmlTest.Load(textBox_Selected_Xml_File.Text);
+
+                //Replaces the node "Orders" with "OrdersByDate" instead.
+                var x = xmlTest.DocumentElement;
+                var newNode = xmlTest.CreateElement("OrdersByDate");
+                newNode.InnerXml = x.LastChild.InnerXml;
+                x.ReplaceChild(newNode, x.LastChild);
+
+                //Save all our nodes in the document in a nodelist.
+                XmlNodeList dataNodes = xmlTest.SelectNodes("/Root");
+
+                //We call on our method to begin conversion.
+                TraverseNodes(dataNodes, ref xmlTest);
+
+                //We save our newly converted XML document to be sorted.
+                xmlTest.Save("tobesorted.xml");
+
+                //We create an LINQ-version of our XML document (this was the only way I was able to do the sorting)
+                XDocument xdoc = XDocument.Load("tobesorted.xml");
+
+                //We call on our sort method, pass on the LINQ-version and prompt the user to select a location for sorted XML file to be saved.                
+                SaveFileDialog sFD = new SaveFileDialog();
+                if (sFD.ShowDialog() == DialogResult.OK)
+                {
+                    SortXmlDocument(xdoc).Save(sFD.FileName);
+                }
+
+                //We communicate to the user that the conversion was successful.
+                label_Conversion_Result.Text = "Conversion successful!";
+            }
+        }
+
+        /// <summary>
+        /// THIS METHOD RECURSIVELY GOES THROUGH ALL NODES AND CREATES AN ATTRIBUTE CALLED "DATE" FOR EACH NODE CONTAINING THE CHILDNODE "ORDERDATE".
+        /// </summary>
+        /// <param name="nodes">List of nodes for method to go through</param>
+        /// <param name="xmlTest">Our XML document we loaded in to be referenced</param>
+        
+        public static void TraverseNodes(XmlNodeList nodes, ref XmlDocument xmlTest)
+        {
+            //We loop through every node in the node list.
+            foreach (XmlNode node in nodes)
+            {
+                //When we find the OrderDate node. 
+                if (node.Name.Equals("OrderDate"))
+                {
+                    //We create a new attribute called "date".
+                    XmlAttribute attr = xmlTest.CreateAttribute("date");
+                    //Copies the content of the node and place it in our new attribute.
+                    attr.Value = node.InnerText;
+                    //We select the parent node (Order) for the node we're currently in (OrderDate), and gives it a new 
+                    ((XmlElement)node.ParentNode).SetAttribute("date", node.InnerText);
+                }
+                //We recursively call on the same method we're in to go through all nodes.
+                TraverseNodes(node.ChildNodes, ref xmlTest);
+            }
+
+        }
+
+        /// <summary>
+        /// THIS METHOD LOOPS THROUGH ALL THE ORDER ELEMENTS AND SORTS THEM BY THEIR ATTRIBUTE "DATE".
+        /// </summary>
+        /// <param name="xdoc">LINQ version of our XML document</param>       
+
+        public static XDocument SortXmlDocument(XDocument xdoc)
+        {
+            //Get the order nodes and place them in a node list called users.
+            var users = xdoc.Elements().Elements("OrdersByDate");
+            foreach (var user in users)
+            {
+                //Recieve the elements called Order, order them by their attribute date and place in an array.
+                var elements = user.Elements("Order").OrderBy(a => a.Attribute("date").Value).ToArray();
+                //We remove the old unsorted nodes from the xml document.
+                user.Elements().Remove();
+                //We add the new sorted nodes to the xml document.
+                user.Add(elements);
+            }
+
+            //Save the new version and return the xml document.
+            xdoc.Save("OrdersByDate.xml");
+            return xdoc;
+        }
+    }
+}
